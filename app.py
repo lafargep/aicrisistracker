510d67e0cf2b90db498328918c3317a17bc29e80
Pier LaFarge <pierlafarge@MacBook-Air-55.local>
2026-03-03 14:53:46 -0600

    Add Google AdSense

--- app.py
+++ app.py
@@ -351,6 +351,8 @@ DASHBOARD_HTML = r"""<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7605456088723739"
+     crossorigin="anonymous"></script>
 <title>Citrini 2028 GIC Tracker</title>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
